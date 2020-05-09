a91daf6624a336b02abc0533b821b34556354c01
Guilherme <guilhermeoliveira564@gmail.com>
2020-05-09 23:58:41 +0200

    Define domain entities

--- src/hotels/core.clj
+++ src/hotels/core.clj
@@ -1,6 +1,7 @@
 (ns hotels.core
   (:require [clojure.string :as str]))
 
+;; Parser
 (defn remove_white_spaces
   [input]
   (str/replace input #"\s+" ""))
@@ -13,12 +14,17 @@
   [input]
   (flatten (mapv #(str/split % #",") input)))
 
-(defn parse_bookings [input] ((comp interpret_booking_dates interpret_customer remove_white_spaces) input))
+(defn parse_bookings
+  [input]
+  ((comp interpret_booking_dates interpret_customer remove_white_spaces) input))
 
-(let [])
+;; Domain
+(defrecord Hotel [name rating rates])
+(defrecord Rates [regular_rate rewards_rate])
+(defrecord Rate [weekend weekday])
+(defrecord Booking [customerType listOfDates])
 
-(def person
-  {:customerType "Kelly"
-   :listOfDates "Keen"
-   :age 32
-   :occupation "Programmer"})
+;; 
+(def Lakewood (Hotel. "Lakewood" 3 (Rates. (Rate. 110 90) (Rate. 80 80))))
+(def Bridgewood (Hotel. "Bridgewood" 4 (Rates. (Rate. 160 60) (Rate. 110 50))))
+(def Ridgewood (Hotel. "Ridgewood" 5 (Rates. (Rate. 220 150) (Rate. 100 40))))
